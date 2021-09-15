8f37a36e87b6977bff94e1ce1df6a240b78dbc5a
Daniel Heery <danieljheery@gmail.com>
2021-09-15 15:33:24 +0100

    Make Liz happy & editing bin/bash to bin/zsh

---
+++ lizFun.sh
@@ -0,0 +1,4 @@
+#!/bin/zsh
+
+echo "Liz is a very beautiful sexy lady & I love her ALOT"
+open "https://wayfair.com" "https://dunelm.com" "https://ikea.com" "https://www.diy.com/" "https://www.bmstores.co.uk/" "https://www.facebook.com/photo/?fbid=10205877546570620&set=a.4672159734775" "https://www.youtube.com/watch?v=h9NGKjn9_kA" "https://www.instagram.com/juniperfoxx/"
