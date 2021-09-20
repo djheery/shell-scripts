25308e4e9e3924e791876ab5602bff165f3e29a9
Daniel <danieljheery@gmail.com>
2021-09-20 19:10:31 +0100

    Day Start

---
+++ dayStart.sh
@@ -0,0 +1,14 @@
+#!/bin/zsh
+
+echo 'Opening Slack'
+open -a slack
+echo 'Opening Web Pages'
+open "https://trello.com" "https://gmail.com" "https://google.com" "https://github.com/djheery"
+echo 'Opening shell scripts'
+cd ~/shs && code .
+sleep .6
+echo 'Smash the shit out of your day'
+sleep 1 
+echo 'Carry those log'
+sleep 1.2 
+echo 'Carry those boats'
