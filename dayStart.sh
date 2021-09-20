d5da9f04779306d614055f3c91df27f1f725c19a
Daniel <danieljheery@gmail.com>
2021-09-20 19:13:25 +0100

    dayStart

--- dayStart.sh
+++ dayStart.sh
@@ -4,11 +4,13 @@ echo 'Opening Slack'
 open -a slack
 echo 'Opening Web Pages'
 open "https://trello.com" "https://gmail.com" "https://google.com" "https://github.com/djheery"
+echo 'Opening Toggl'
+open -a 'toggl track'
 echo 'Opening shell scripts'
 cd ~/shs && code .
 sleep .6
 echo 'Smash the shit out of your day'
 sleep 1 
-echo 'Carry those log'
+echo 'Carry those logs'
 sleep 1.2 
 echo 'Carry those boats'
